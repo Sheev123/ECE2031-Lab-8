01b3d27f2ada18de2b478f7673a27af58475c8f0
Ruiz <druiz35@gatech.edu>
2025-07-14 18:30:17 -0400

    Pushing Hayden's Parser edits

--- SCOMP_Lab8_Sum25/PARSE_DISP_DATA_IN.vhd
+++ SCOMP_Lab8_Sum25/PARSE_DISP_DATA_IN.vhd
@@ -9,9 +9,8 @@ entity PARSE_DISP_DATA_IN is
         IO_DATA  : in  std_logic_vector(15 downto 0);-- data bus
 
         MODE      : out std_logic_vector(1 downto 0); -- 00=h,01=b,10=d,11=fc
-        FC_ENABLE : out std_logic;                    -- full‑control master enable
-        FC_DISP   : out std_logic_vector(2 downto 0); -- which digit (0‑5)
-        FC_SEG    : out std_logic_vector(2 downto 0); -- which segment
+        FC_VALUE  : out std_logic_vector(6 downto 0);
+		  FC_DISPLAY_SELECT : out std_logic_vector(2 downto 0);
 		  
         GRP0_VALUE : out std_logic_vector(12 downto 0); -- 4‑digit value
         GRP0_WRITE : out std_logic;                      -- write pulse for grp0
@@ -25,10 +24,9 @@ architecture comb of PARSE_DISP_DATA_IN is
     constant MODE_FC : std_logic_vector(1 downto 0) := "11";
 
     signal mode_s               : std_logic_vector(1 downto 0); -- 15‑14
-    signal full_disp_override_s : std_logic;                     -- 13
-    signal fc_enable_s          : std_logic;                     -- 12
-    signal fc_disp_s            : std_logic_vector(2 downto 0); -- 11‑9
-    signal fc_seg_s             : std_logic_vector(2 downto 0); --  8‑6
+    signal full_disp_override_s : std_logic;                    -- 13
+    signal full_disp_value_s    : std_logic_vector(6 downto 0); -- 6-0
+	 signal full_disp_display_s  : std_logic_vector(2 downto 0); -- 13-11
     signal value_s              : std_logic_vector(12 downto 0);-- 12‑0
 
     -- Write enables
@@ -38,26 +36,27 @@ begin
     -- Split the incoming data word once
     mode_s               <= IO_DATA(15 downto 14);
     full_disp_override_s <= IO_DATA(13);
-    fc_enable_s          <= IO_DATA(12);
-    fc_disp_s            <= IO_DATA(11 downto 9);
-    fc_seg_s             <= IO_DATA(8  downto 6);
+	 full_disp_value_s 	 <= IO_DATA(6 downto 0);
+	 full_disp_display_s  <= IO_DATA(13 downto 9);
     value_s              <= IO_DATA(12 downto 0);
 
     -- Shared outputs (identical for both display banks)
     MODE      <= mode_s;
-    FC_ENABLE <= fc_enable_s when mode_s = MODE_FC else '0';
-    FC_DISP   <= fc_disp_s   when mode_s = MODE_FC else (others => '0');
-    FC_SEG    <= fc_seg_s    when mode_s = MODE_FC else (others => '0');
+	 FC_VALUE  <= full_disp_value_s when mode_s = MODE_FC else (others => '0');
+	 FC_DISPLAY_SELECT <= full_disp_display_s when mode_s = MODE_FC else (others => '0');
 
     -- Write‑enable generation
     -- addr 4: four‑digit group (grp0)
     -- addr 5: two‑digit group (grp1)
-    -- full_disp_override extends addr 4 write to grp1 as well
-    grp0_we_s <= IO_WRITE when IO_ADDR = "00000000100" else '0'; -- 4
-
+    -- full_disp_override extends addr 4 and 5 write to all groups
+    grp1_we_s <= IO_WRITE when (IO_ADDR = "00000000100") or      -- 4
+                               (IO_ADDR = "00000000101" and full_disp_override_s = '1') 
+					else '0';
+										 
     grp1_we_s <= IO_WRITE when (IO_ADDR = "00000000101") or      -- 5
-                               (IO_ADDR = "00000000100" and full_disp_override_s = '1')
-                 else '0';
+                               (IO_ADDR = "00000000100" and full_disp_override_s = '1') 
+										 -- if overriding, also set grp1 WE
+               else '0';
 
     GRP0_WRITE <= grp0_we_s;
     GRP1_WRITE <= grp1_we_s;
@@ -67,8 +66,4 @@ begin
 	 -- grp1 will not output a value when in full 6-display mode, grp0 will have the value
 	 -- otherwise, both values are determined by their write-enable flag which is determined
 	 -- by the device we out to, just like in Lab 8.
-	 
-	 -- Note for full control: Even though we're outputing 13 bits, the first few are reserved
-	 -- for additional flags as fc_*
-
 end architecture comb;
