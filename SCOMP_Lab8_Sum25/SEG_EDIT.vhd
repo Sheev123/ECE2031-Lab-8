94f8b48d181bcf72b341f9d4b1bfef9ac13ed534
druiz365 <156946917+druiz365@users.noreply.github.com>
2025-07-14 22:19:10 -0400

    Update SEG_EDIT.vhd to allow singling out individual segments based on FC_DISP

--- SCOMP_Lab8_Sum25/SEG_EDIT.vhd
+++ SCOMP_Lab8_Sum25/SEG_EDIT.vhd
@@ -4,48 +4,32 @@ use IEEE.numeric_std.all;
 
 entity SEG_EDIT is
     port (
-        EDIT_DISP       : in std_logic_vector(2 downto 0); -- which digit (0‑5)
-        EDIT_SEG        : in std_logic_vector(2 downto 0); -- which segment
-
-
-
+        FC_DISP         : in std_logic_vector(2 downto 0); -- which HEX (0‑5)
+        FC_VALUE        : in std_logic_vector(6 downto 0); -- what value
+        segments        : out std_logic_vector(41 downto 0) -- output
     );
 end entity SEG_EDIT;
 
 architecture a of SEG_EDIT is
-    -- Individual HEX signals
-    signal HEX0               : std_logic_vector(6 downto 0);
-    signal HEX1               : std_logic_vector(6 downto 0);
-    signal HEX2               : std_logic_vector(6 downto 0);
-    signal HEX3               : std_logic_vector(6 downto 0);
-    signal HEX4               : std_logic_vector(6 downto 0);
-    signal HEX5               : std_logic_vector(6 downto 0);
-
-    -- HEX mask to edit individual segment
-    signal mask               : std_logic_vector(6 downto 0);
-
-    -- RESET and SET bit vectors
-    constant RESET            : std_logic_vector(41 downto 0) := (others => '0');
-    constant SET              : std_logic_vector(41 downto 0) := (others => '1');
-
-begin
-
-    process(MODE, segments_in, DLT, EDIT_DISP, EDIT_SEG)
-        variable idx : integer;
     begin
-        
+    process(FC_DISP, FC_VALUE)
+    begin
+        case FC_DISP is
+            when "000" =>
+                segments <= "11111111111111111111111111111111111" & FC_VALUE;
+            when "001" =>
+                segments <= "1111111111111111111111111111" & FC_VALUE & "1111111";
+            when "010" =>
+                segments <= "111111111111111111111" & FC_VALUE & "11111111111111";
+            when "100" =>
+                segments <= "11111111111111" & FC_VALUE & "111111111111111111111";
+            when "101" =>
+                segments <= "1111111" & FC_VALUE & "1111111111111111111111111111";
+            when "110" =>
+                segments <= FC_VALUE & "11111111111111111111111111111111111";
+            when others =>
+                segments <= "000000000000000000000000000000000000000000";
+        end case;
 
-		-- Create bitmask to edit segment
-		idx := to_integer(unsigned(EDIT_SEG));
-
-		for i in 0 to 6 loop
-			 if i = idx then 
-				  mask(i) <= '0';
-			 else
-				  mask(i) <= '1';
-			 end if;
-		end loop;
-
-
- end process;
+    end process;
 end architecture a;
