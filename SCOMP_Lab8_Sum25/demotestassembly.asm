a729309b2fdf17be174fa5e92b6d1c81d6289eaf
ifeanyi33 <iezegbo3@gatech.edu>
2025-07-17 19:03:27 -0400

    Add files via upload

---
+++ SCOMP_Lab8_Sum25/demotestassembly.asm
@@ -0,0 +1,107 @@
+; HexPeripheralTest.asm
+        ORG     0
+
+;Value Modes with and without Override
+
+
+
+;Full Control Mode
+;Loop through digits 0 to 5 with one segment pattern
+
+        LOADI   6
+        STORE   DigitIndex
+
+DigitLoop:
+        LOAD   DigitIndex
+        ;ADDI    -6
+        ;JNEG   Done
+
+        ;shift digit index into bits 13:11
+        LOAD    DigitIndex
+        SHIFT   11
+        STORE   TempDigitBits
+CHECK_IF_EVEN:
+		LOAD 	DigitIndex
+        AND 	EvenCheck
+        SUB		EvenCheck
+        JNEG    Pattern1
+        JZERO 	Pattern2
+Pattern2:
+		LOAD    SegmentPattern2
+        STORE   TempSegBits
+        JUMP 	Continue
+
+Pattern1:
+        ; Load segment pattern(&B01010100)
+        LOAD    SegmentPattern1
+        STORE   TempSegBits
+Continue:
+        ;combine digit bits + segment bits
+        LOAD    TempDigitBits
+        ADD     TempSegBits
+        STORE   TempCombined
+
+        ; add mode bits (Mode = 11, override = 0)
+        LOAD    FullCtrlModeBits
+        ADD     TempCombined
+        STORE   FullCtrlWord
+
+        ;write to HEX0
+        LOAD    FullCtrlWord
+        OUT     Hex0
+
+        CALL    Delay
+
+        ;increment digit index and loop
+        LOAD    DigitIndex
+        ADDI    -1
+        JNEG	RESET
+        STORE   DigitIndex
+        JUMP    DigitLoop
+RESET:
+		
+		LOADI 6
+        STORE DigitIndex
+        JUMP    DigitLoop
+
+Done:
+		JUMP 	Done
+Delay:
+        OUT     Timer
+WaitLoop:
+        IN      Timer
+        ADDI    -5
+        JNEG    WaitLoop
+        RETURN
+
+;Test Values for Value test
+
+ValHexNoOverride:     DW &B0000000100100011   ; MODE=00, override=0, value=&H123
+ValHexOverride:       DW &B0010000100100011   ; MODE=00, override=1, value=&H123
+
+ValDecNoOverride:     DW &B0100000000001010   ; MODE=01, override=0, value=10
+ValDecOverride:       DW &B0110000000001010   ; MODE=01, override=1, value=10
+
+ValBinNoOverride:     DW &B1000000000001111   ; MODE=10, override=0, value=15
+ValBinOverride:       DW &B1010000000001111   ; MODE=10, override=1, value=15
+
+;Segment Pattern for Full Control Test 
+SegmentPattern1:       DW &B0000000000011100 
+SegmentPattern2:       DW &B0000000000100011
+
+;I/O Addresses
+Hex0:         EQU &H04
+Hex1:         EQU &H05
+Timer:        EQU &H02
+
+;Temporary Registers
+DigitIndex:     DW 0
+TempDigitBits:  DW 0
+TempSegBits:    DW 0
+TempCombined:   DW 0
+FullCtrlWord:   DW 0
+
+;Constants
+FullCtrlModeBits: DW &HC000
+EvenCheck:		  DW &B0000000000000001
+
